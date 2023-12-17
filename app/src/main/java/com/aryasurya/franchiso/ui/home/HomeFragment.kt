89fc61822305a542afe4ac707cb762babc0ea0ac
AryaSurya <suryasaputra914@gmail.com>
2023-12-17 20:52:27 +0700

    get data dari user yang login dan menampilkannya di menu home, menggunakan refreshswipelayout untuk mendapatkan data yang baru saja di upload

--- app/src/main/java/com/aryasurya/franchiso/ui/home/HomeFragment.kt
+++ app/src/main/java/com/aryasurya/franchiso/ui/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.aryasurya.franchiso.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,36 +36,79 @@ class HomeFragment : Fragment() {
 
         sessionManager = SessionManager(requireContext())
 
+//        val db = FirebaseFirestore.getInstance()
+//        val franchisesCollection = db.collection("franchises")
+//
+//        franchisesCollection.get()
+//            .addOnSuccessListener { documents ->
+//                val franchiseList = mutableListOf<FranchiseData>()
+//
+//                for (document in documents) {
+//                    val franchiseData = document.toObject(FranchiseData::class.java)
+//                    franchiseList.add(franchiseData)
+//                }
+//
+//                // Set up RecyclerView dengan adapter untuk menampilkan data franchises
+//                val adapter = FranchiseAdapter(franchiseList)
+//                binding.rvListStory.adapter = adapter
+//                binding.rvListStory.layoutManager = LinearLayoutManager(requireContext())
+//            }
+//            .addOnFailureListener { exception ->
+//                // Handle error jika gagal mengambil data dari Firestore
+//            }
+
+        val swipeRefreshLayout = binding.swipeRefreshLayout
+
+        // Tambahkan listener untuk refresh
+        swipeRefreshLayout.setOnRefreshListener {
+            // Panggil fungsi untuk memuat ulang data
+            loadData()
+        }
+
+        // ...
+
+        // Memuat data pertama kali ketika fragment dimuat
+        loadData()
+
+    }
+
+    private fun loadData() {
+
+
         val user: User? = sessionManager.getSession()
 
         // Lakukan pengecekan apakah ada sesi pengguna atau tidak
         if (user != null) {
 
+            val userId = user.userId // Ganti dengan userId yang ingin Anda gunakan
+            val db = FirebaseFirestore.getInstance()
+            val franchisesCollection = db.collection("franchises")
+
+            // Membuat query untuk mendapatkan data dengan userId tertentu
+            franchisesCollection.whereEqualTo("userId", userId)
+                .get()
+                .addOnSuccessListener { documents ->
+                    val franchiseList = mutableListOf<FranchiseData>()
+
+                    for (document in documents) {
+                        val franchiseData = document.toObject(FranchiseData::class.java)
+                        franchiseList.add(franchiseData)
+                    }
+                    // Set up RecyclerView dengan adapter untuk menampilkan data franchises
+                    val adapter = FranchiseAdapter(franchiseList)
+                    binding.rvListStory.adapter = adapter
+                    binding.rvListStory.layoutManager = LinearLayoutManager(requireContext())
+
+                    binding.swipeRefreshLayout.isRefreshing = false
+                }
+                .addOnFailureListener { exception ->
+                    // Handle ketika terjadi kesalahan
+                    Log.e("Firestore", "Error getting documents: ", exception)
+                }
+
         } else {
             startActivity(Intent(requireActivity(), LoginActivity::class.java))
             requireActivity().finish()
         }
-
-        val db = FirebaseFirestore.getInstance()
-        val franchisesCollection = db.collection("franchises")
-
-        franchisesCollection.get()
-            .addOnSuccessListener { documents ->
-                val franchiseList = mutableListOf<FranchiseData>()
-
-                for (document in documents) {
-                    val franchiseData = document.toObject(FranchiseData::class.java)
-                    franchiseList.add(franchiseData)
-                }
-
-                // Set up RecyclerView dengan adapter untuk menampilkan data franchises
-                val adapter = FranchiseAdapter(franchiseList)
-                binding.rvListStory.adapter = adapter
-                binding.rvListStory.layoutManager = LinearLayoutManager(requireContext())
-            }
-            .addOnFailureListener { exception ->
-                // Handle error jika gagal mengambil data dari Firestore
-            }
-
     }
 }
